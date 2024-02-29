90a61a767be884912482f898bb2f14fb93afaa77
anuardosmukhamed87 <anuardosmukhamed87@gmail.com>
2024-02-29 14:25:18 +0600

    First commit

--- birdsSite/settings.py
+++ birdsSite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7g0gg_byq6f@3_u1r_*3ak9zuweznu^7ldj^ppg!n)^es+_ri4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
